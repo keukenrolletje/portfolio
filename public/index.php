013af1e12c973fd46229919a1e1e5d2c3f86e73b
Joke Gysen <jokegysen@gmail.com>
2015-07-01 13:20:50 +0200

    Create project: jokegysen Portfolio

---
+++ public/index.php
@@ -0,0 +1,16 @@
+<!--add vars for page active navbar-->
+<?php include 'partials/header.php'; ?>
+
+<div class="roundwrapper">
+    <div class="round">
+        <div class="circle-text">
+            <div>
+                <h1>Hi!</h1>
+                <h5>I am Joke Gysen, </br></h5>
+                <h5>a 25 years old, self-taught web designer from Antwerp, Belgium.</h5>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'partials/footer.php'; ?>
---
+++ public/partials/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Joke Gysen - Front-End Developer</title>
+    <link rel="stylesheet" href="stylesheets/main.css" type="text/css" />
+    <link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,700italic|Anaheim|Muli' rel='stylesheet' type='text/css'>
+    <script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+</head>
+
+<body id="index" class="home">
+<div class="wrapper">
+    <header id="topnav" class="navbar">
+        <div class="navwrapper">
+            <nav id="mobilenav">
+                <ul>
+                    <li><a href="#" class="sel">home</a></li>
+                    <li><a href="#">about me</a></li>
+                    <li><a href="index.html" class="logo"><img src="/../images/logo/logo60.png" alt="logo"></a></li>
+                    <li><a href="#" class="special">portfolio</a></li>
+                    <li><a href="#">contact</a></li>
+                </ul>
+            </nav>
+        </div>
+        <!--<div id="btncolor">
+            <a href="#" id="navbtn">Nav Menu</a>
+        </div>-->
+    </header>
