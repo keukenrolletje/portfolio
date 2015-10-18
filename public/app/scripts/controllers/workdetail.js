ef72dc626d4ae6da8ca3cb89d3e671faf4878ca7
Joke Gysen <jokegysen@gmail.com>
2015-10-18 16:20:22 +0200

    Allow html and script tags to pass

--- public/app/scripts/controllers/workdetail.js
+++ public/app/scripts/controllers/workdetail.js
@@ -1,14 +1,37 @@
 'use strict';
 
 var App = angular.module('publicApp');
-App.controller('workdetailCtrl', [ '$scope', '$routeParams', '$http', function ($scope, $routeParams, $http) {
+App.controller('workdetailCtrl', ['$rootScope', '$scope', '$routeParams', '$http', function ($rootScope, $scope, $routeParams, $http) {
 
 
 	$http.get('portfolio/' + $routeParams.id + '.json')
        .then(function(res){
-          $scope.work = res.data;
-          //$scope.works = res.data.splice(0, 5);       
+          $scope.work = res.data;     
          $rootScope.exp = $scope.work.tag;
-        });
+    });
 
-  }]);
+    
+    
+
+  }])
+.directive('html', [ function () {
+  return {
+    restrict: 'A',
+    link: function (scope, element, attrs) {
+      element.html(attrs.html);
+    }
+  };
+}])
+.directive('iframecss', [function(){
+	return{
+		restrict: 'A',
+		link: function(scope, element){
+			element.contents().find('.embed-nav').css({
+			    opacity: 1,
+			    color: 'purple'
+			});
+		}
+	};
+}])
+;
+/*using directive to get html and script from json file. Html works with html-bind but script didn't work.*/
