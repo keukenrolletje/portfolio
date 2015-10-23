d855ad1a6ea0a66da2b7fb2960ef2d8da9ba1afd
Joke Gysen <jokegysen@gmail.com>
2015-10-23 20:36:25 +0200

    Add randomNumber directive

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -21,7 +21,7 @@ angular
     'appAnimations', 
     'duParallax',
     'duScroll'
-  ])
+      ])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
@@ -50,8 +50,23 @@ angular
         redirectTo:'/'
       })*/
       ;
-    
 
      // $locationProvider.html5Mode(true);
      // $location.Html5mode(true).hashbang("!");
-  });
+  })
+  .directive('randomnumber', function(){
+    return {
+      restrict: 'A',
+      link: function($rootScope){
+       var navbutton = angular.element('.menu-btn');
+       navbutton.bind('click', function(){
+          $rootScope.random = [];
+          for (var i=0; i<7; i++) {
+            var randomNbr = Math.random() * (0.7 - 0.1) + 0.1;
+            $rootScope.random.push(randomNbr);
+          }
+       });
+      }
+  };
+})
+  ;
