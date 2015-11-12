00d87831ce2a7a9ab67c0916b51da4963ed44ee6
Joke Gysen <jokegysen@gmail.com>
2015-11-12 14:02:50 +0100

    Make resize on embed possible

--- app/scripts/controllers/workdetail.js
+++ app/scripts/controllers/workdetail.js
@@ -1,13 +1,21 @@
 'use strict';
 
 var App = angular.module('publicApp');
-App.controller('workdetailCtrl', ['$rootScope', '$scope', '$routeParams', '$http', 'messages', function ($rootScope, $scope, $routeParams, $http, messages) {
+App.controller('workdetailCtrl', ['$rootScope', '$scope', '$routeParams', '$http', function ($rootScope, $scope, $routeParams, $http) {
 
 
 	$http.get('portfolio/' + $routeParams.id + '.json')
        .then(function(res){
-          $scope.work = res.data;     
+          $scope.work = res.data;  
+
+          if(window.innerWidth > 1559){
+            var str = JSON.stringify($scope.work.code);
+            var stringified = str.replace('510', '1400');
+            $scope.work.code = JSON.parse(stringified);
+          }   
     });
+
+       
 
   }])
 .directive('html', [ function () {
